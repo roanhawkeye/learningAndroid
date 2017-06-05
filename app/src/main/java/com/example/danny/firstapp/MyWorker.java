1d3da3d574824628e9910e3b30e1b092f8a4c6a3
Daniel Martinez <dmartinez@itexico.net>
2017-06-05 15:25:44 -0500

    Initial commmit

---
+++ app/src/main/java/com/example/danny/firstapp/MyWorker.java
@@ -0,0 +1,11 @@
+package com.example.danny.firstapp;
+
+/**
+ * Created by danny on 05/06/17.
+ */
+
+public class MyWorker {
+    public static int doubleTheValue(int value) {
+        return value * 2;
+    }
+}
